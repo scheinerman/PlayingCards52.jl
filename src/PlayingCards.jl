d0b959c1f61b65b7f7af3b4fde3384ad3e97829d
Ed Scheinerman <ers@jhu.edu>
2020-07-20 12:56:36 -0400

    Suit symbols

--- src/PlayingCards.jl
+++ src/PlayingCards.jl
@@ -4,10 +4,14 @@ module PlayingCards
 suit_list = [ :clubs, :diamonds, :hearts, :spades ]
 suit_set = Set(suit_list)
 
+suit_char = Dict{Symbol,Char}(:clubs => '♣', :diamonds => '♢',
+    :hearts => '♡', :spades => '♠')
+
+
 export Suit
 
 struct Suit
-    suit::Symbol
+    s::Symbol
     function Suit(s::Symbol)
         if !in(s,suit_list)
             error("No such suit $s.\nUse one of these: $suit_list.")
@@ -15,5 +19,21 @@ struct Suit
         new(s)
     end
 end
+
+struct Card
+    suit::Suit
+    rnk::Int
+    function Card(s::Symbol, r::Int)
+        if r<1 || r>13
+            error("Card rank must be between 1 (ace) and 13 (king)")
+        end
+        new(Suit(s),r)
+    end
+end
+
+export Card
+
+
+
 
 end # module
